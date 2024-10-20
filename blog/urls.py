cb225d991c927d75d2e77770a517bf9a1a98e521
zech <zechguo30@hotmail.com>
2024-10-20 21:40:48 +0800

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
 # ignore domain name, match empty string, tells Django to go to views.post_list 
 # if someone enters the website at the 'http://127.0.0.1:8000/' address.
 # post_list is the name of the URL used to identify the view
+
+# <int:pk>: Django expects an integer value and will transfer it to a view as a variable called pk.
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,9 +7,16 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 # takes request and will return the value it gets from calling render that will render 
 # (put together) the template blog/post_list.html.
 # Params:
 # request: everything we receive from the user via the Internet
 # 'blog/post_list.html': template file 
 # {'posts': posts}: things for the template to use
+
+# get_object_or_404:  In case there is no Post with the given pk, it will display much nicer page,
+# the Page Not Found 404 page.
